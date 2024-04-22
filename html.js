0908a7136799e3e3b7232d976f1624fdf377f3be
jacob <ghday.jacob@gmail.com>
2024-04-22 21:36:15 +0200

    uppdatering

---
+++ html.js
@@ -0,0 +1,48 @@
+// window.onload = function () {
+//     document.querySelector('h1').style.color = 'Blue';
+// };
+
+
+// ctrl + /
+// window.alert('Hello Jag heter Jacob');
+// document.write('<h1>Hello<span>Jacob</span></h1>');
+// console.log('Hello Jacob');
+
+// console.log('Hello');
+// console.error('Error');
+// console.table(['jacob','Ammar', 'Ana']);
+// console.log('%cJacob %cGhday', 'color:red; font-size:40px', 'color:blue; font-size:50px');
+
+/*
+--ES6
+*/
+// let jacob = 'Jacob';
+// console.log('Hello' +' '+ jacob);
+// console.log(`hello ${jacob}`);
+
+
+
+
+// Data Tupe
+// String
+// Number
+// Arry => Object
+// Object
+//  Boolean
+// Undefined
+
+
+
+// console.log('Jacob');
+// console.log(typeof 'Jacob');
+// console.log(typeof 800);
+// console.log(typeof 90.10);
+// console.log(typeof [10, 14, 18]);
+// console.log(typeof ['as', 'ah', 'la']);
+// console.log(typeof { name : 'Jacob ', age : 12, Land: 'Sy'});
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log( typeof undefined);
+// console.log(typeof null);
+
+
